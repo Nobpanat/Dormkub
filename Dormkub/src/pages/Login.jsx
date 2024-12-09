83e5d78293f917abafe91def1b84c1bcfb942966
nite <noppana08@gmail.com>
2024-12-09 11:51:15 +0700

    decorrate login page

--- Dormkub/src/pages/Login.jsx
+++ Dormkub/src/pages/Login.jsx
@@ -1,28 +1,47 @@
-// src/pages/Loginjsx
+import { Link } from "react-router-dom";
 
-import {Link} from 'react-router-dom';
+// import img logo
+import logo from "../assets/images/Frame 84@2x.png";
 
-const Login = () => {
-    const handleGoogleLogin = () => {
-        window.location.href = 'http://localhost:5000/auth/google';
-    };
+import GoogleSignInButton from "../components/GoogleSignInButton";
+import Footer from "../components/Footer";
 
+const LoginPage = () => {
+  return (
+    <div className="h-screen bg-gray-100">
+      {/* Header */}
+      <header className="bg-[#03045E] text-white flex items-center justify-normal px-4 py-2 gap-20">
+        <div className="flex items-center space-x-2">
+          {/* เพิ่ม logo */}
+          <Link to="/">
+            <img src={logo} alt="Logo" className="w-20 h-10 lg:w-24 lg:h-14" />
+          </Link>
+        </div>
+        <div className="text-lg">เข้าสู่ระบบ</div>
+      </header>
 
-    return (
-        <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
-        <h2 className="text-2xl mb-4">เข้าสู่ระบบด้วย Google</h2>
-        <p className="mb-6">คุณสามารถใช้ Google Account เพื่อเข้าสู่ระบบ</p>
-        <button
-          onClick={handleGoogleLogin}
-          className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
-        >
-          Login with Google
-        </button>
-        <Link to="/" className="mt-4 text-blue-500 hover:underline">
-          กลับไปหน้าหลัก
-        </Link>
-      </div> 
-    );
+      {/* Main Content */}
+      <main className="flex justify-center items-center h-full">
+        <div className="border border-gray-300 shadow-md rounded-md w-[600px] h-[300px] flex items-center p-4">
+          {/* Left Section */}
+          <div className="flex-1 flex flex-col items-center">
+          <img src={logo} alt="Logo" className="w-32 h-24" />
+            {/* <div className="h-24 w-24 bg-gray-300 mb-4" /> */}
+            
+          </div>
+
+          {/* Right Section */}
+          <div className="flex-1 flex flex-col items-center justify-center">
+            {/* <h3 className="text-base font-semibold mb-2">เข้าสู่ระบบด้วย</h3> */}
+            <GoogleSignInButton />
+          </div>
+        </div>
+      </main>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
 };
 
-export default Login;
+export default LoginPage;
